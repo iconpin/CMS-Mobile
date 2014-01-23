aa0d8014ad488dc54f75c31075fcefd790443ced
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-01-23 11:05:17 +0100

    Add salutation to CMS::Views::Home

--- views/home.rb
+++ views/home.rb
@@ -2,7 +2,11 @@ class CMS
   module Views
     class Home < Layout
       def content
-        "Welcome. We're running Sinatra + Mustache"
+        if user.nil?
+          "Welcome. We're running Sinatra + Mustache"
+        else
+          "Welcome #{user}. We're running Sinatra + Mustache"
+        end
       end
     end
   end
