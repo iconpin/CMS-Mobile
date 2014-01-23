3ece081b1d653c90fb7015066256f1e03339edb3
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-01-23 10:24:54 +0100

    Update CMS::Models::User (validations)

--- models.rb
+++ models.rb
@@ -6,8 +6,19 @@ class CMS
       include DataMapper::Resource
       property :id, Serial
       property :name, String
+      property :email, String
       property :password, BCryptHash
       property :created_at, DateTime
+
+      validates_presence_of :name
+      validates_uniqueness_of :name
+
+      validaes_uniqueness_of :email
+      validates_presence_of :email
+      validates_format_of :email, :as => :email_address
+
+      validates_presence_of :password
+      validates_length_of :password, :min => 5
     end
   end
 end
