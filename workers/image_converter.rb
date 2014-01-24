419ce6bdaf3d5c1d05dc9817e61e1c02a1f19da7
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-01-24 13:34:41 +0100

    Add first naïve impl. of Workers::ImageConverter

---
+++ workers/image_converter.rb
@@ -0,0 +1,19 @@
+require 'sidekiq'
+require 'minimagick'
+
+class CMS
+  module Workers
+    class ImageConverter
+      include Sidekiq::Worker
+
+      def perform(db_image)
+        image = MiniMagick::Image.open(db_image.path_tmp)
+        image.resize("200x200")
+        image.write(db_image.path)
+
+        db_image.ready = true
+        db_image.save
+      end
+    end
+  end
+end
