2df68488c489633155463cfa79d3bf3dae717c02
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-01-24 14:08:15 +0100

    Update the ImageConverter to record the error
    Update the Multimedia model to allow that

--- models/multimedia.rb
+++ models/multimedia.rb
@@ -9,6 +9,7 @@ class CMS
       property :path_tmp, FilePath, :required => true
       property :path, FilePath
       property :ready, Boolean, :required => true, :default => false
+      property :error, Text
       property :description, Text, :required => false
       property :weight, Integer, :required => true, :default => 0
       property :published, Boolean, :required => true, :default => false
--- workers/image_converter.rb
+++ workers/image_converter.rb
@@ -5,8 +5,16 @@ class CMS
   module Workers
     class ImageConverter
       include Sidekiq::Worker
+      sidekiq_options :retry => 3
+
+      sidekiq_retries_exhausted do |msg|
+        image_db = CMS::Models::Image.get(@image_id)
+        image_db.ready = false
+        image_db.error = msg['error_message']
+      end
 
       def perform(image_id)
+        @image_id = image_id
         image_db = CMS::Models::Image.get(image_id)
         image = MiniMagick::Image.open(image_db.path_tmp)
         image.resize("200x200")
