fc057bd72e867fdb5e2370bd00544e7fd59d8831
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-01-27 16:05:01 +0100

    Update Multimedia view and template

--- views/multimedia.rb
+++ views/multimedia.rb
@@ -1,6 +1,21 @@
 class CMS
   module Views
     class Multimedia < Layout
+      def list_multimedia
+        list = []
+        CMS::Models::Multimedia.all.each do |m|
+          list << {
+            :weight => m.weight,
+            :name => m.name,
+            :description => m.description[0..50],
+            # TODO: maybe there's a better way to add the time zone offset?
+            :created_at => (m.created_at + Rational(1, 24)).strftime("%Y/%m/%d %H:%M:%S"),
+            :updated_at =>  (m.updated_at + Rational(1, 24)).strftime("%Y/%m/%d %H:%M:%S"),
+            :published => m.published,
+          }
+        end
+        list
+      end
     end
   end
 end
