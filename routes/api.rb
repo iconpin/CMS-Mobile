90b99c60531e668475aeae40c756e4176fc8e91b
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-02-24 11:12:01 +0100

    Add Routes::API::{'/api/extra/:id', 'api/all'}

--- routes/api.rb
+++ routes/api.rb
@@ -57,9 +57,75 @@ module CMS
             end
             xml.extras do
               p.extras_sorted.each_with_index do |e, i|
-                xml.extra :order => i + 1, :id => e.id, :name => e.name do
-                  xml.description e.description
-                  xml.url link_to("/api/extra/#{e.id}")
+                xml.extra({
+                  :order => i + 1,
+                  :id => e.id,
+                  :name => e.name,
+                  :url => link_to("/api/extra/#{e.id}")
+                })
+              end
+            end
+          end
+        end
+      end
+
+      get '/api/extra/:id' do |id|
+        e = CMS::Models::Extra.get(id)
+        builder do |xml|
+          xml.instruct!
+          xml.extra :name => e.name, :id => e.id do
+            xml.description e.description
+            xml.multimedias do
+              e.multimedias_sorted.each_with_index do |m, i|
+                xml.multimedia :order => i + 1, :id => m.id, :name => m.name do
+                  xml.description m.description
+                  xml.tip m.tip
+                  xml.url link_to(m.static_link)
+                end
+              end
+            end
+          end
+        end
+      end
+
+      get '/api/all' do
+        builder do |xml|
+          xml.instruct!
+          xml.points do
+            CMS::Models::Point.all_sorted.published.not_deleted.each_with_index do |point, i|
+              xml.point :order => i + 1, :id => point.id do
+                xml.name point.name
+                xml.description point.description
+                xml.tip point.tip
+                xml.multimedias do
+                  point.multimedias_sorted.published.each_with_index do |multimedia, i|
+                    xml.multimedia :order => i + 1, :id => multimedia.id do
+                      xml.name multimedia.name
+                      xml.description multimedia.description
+                      xml.tip multimedia.tip
+                      xml.url link_to(multimedia.static_link)
+                    end
+                  end
+                end
+
+                xml.extras do
+                  point.extras_sorted.published.not_deleted.each_with_index do |extra, i|
+                    xml.extra :order => i + 1, :id => extra.id do
+                      xml.name extra.name
+                      xml.description extra.description
+                      xml.tip extra.tip
+                      xml.multimedias do
+                        extra.multimedia_sorted.each_with_index do |multimedia, i|
+                          xml.multimedia :order => i + 1, :id => multimedia.id do
+                            xml.name multimedia.name
+                            xml.description multimedia.description
+                            xml.tip multimedia.tip
+                            xml.url link_to(multimedia.static_link)
+                          end
+                        end
+                      end
+                    end
+                  end
                 end
               end
             end
