cae48cd3f614ca377f6f9b8102275f41f49a525a
Contreras Pinilla, Ignacio <ignacio.contreras@i2cat.net>
2014-03-20 12:36:21 +0100

    Fix paths used internally by the models

--- config/storage.rb
+++ config/storage.rb
@@ -1,6 +1,16 @@
 module CMS
   # Storage config
-  MULTIMEDIA_DIR = File.join(App.root,'/storage/multimedia')
-  TMP_DIR = File.join(App.root, '/storage/tmp')
-  THUMBNAIL_DIR = File.join(App.root, '/storage/thumbnail')
+  multimedia_dir = File.join(App.root,'/storage/multimedia')
+  tmp_dir = File.join(App.root, '/storage/tmp')
+  thumbnail_dir = File.join(App.root, '/storage/thumbnail')
+
+  # Normalization of paths
+  def self.normalize path
+    root = Pathname(CMS::App.root)
+    Pathname(path).relative_path_from(root).to_s
+  end
+
+  MULTIMEDIA_DIR = normalize(multimedia_dir)
+  TMP_DIR = normalize(tmp_dir)
+  THUMBNAIL_DIR = normalize(thumbnail_dir)
 end
